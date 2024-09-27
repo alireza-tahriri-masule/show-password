bd6cc26ace186ad758930221b754b5e838e3be61
Alireza Tahriri Masule <alirezatahriri4@gmail.com>
2024-09-28 03:08:23 +0330

    perf(app): Remove comment Remove all comment
    Remove all comments and make it ready to publish
    
    Closes: Remove comment

--- main.js
+++ main.js
@@ -1,14 +1,10 @@
-// انتخاب تمام input هایی که نوع آنها password است
 const passwordInputs = document.querySelectorAll('input[type="password"]');
 
-// حلقه روی هر input
 passwordInputs.forEach(input => {
-    // زمانی که موس وارد input شد، نوع را به text تغییر بده
     input.addEventListener('mouseenter', () => {
         input.type = 'text';
     });
 
-    // زمانی که موس از input خارج شد، نوع را به password برگردان
     input.addEventListener('mouseleave', () => {
         input.type = 'password';
     });
